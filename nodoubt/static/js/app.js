05e94621142ec762074c6d6960dde257b9663fe0
YoonHwe <dbsghl676@gmail.com>
2021-08-09 12:22:04 +0900

    app.js 수정 및 post 사진 삭제

--- nodoubt/static/js/app.js
+++ nodoubt/static/js/app.js
@@ -19,11 +19,11 @@ function paintList(event){
         category_second.style.color = "black";
         category_third.style.color = "black";
         category_fourth.style.color = "black";
-        first.innerHTML = "<a class='list_content_link' href='https://job.dongguk.edu:450/' target='_blank'><h2>동국대학교 취업센터</h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>학교 공식 채용 정보 제공 홈페이지, 교내 취업프로그램 및 교외 공모전 정보 활용 가능</h3>";
-        second.innerHTML = "<a class='list_content_link' href='https://www.dongguk.edu/mbs/kr/download/job/job_roadmap/roadmap_main.html' target='_blank'><h2>동국대학교 취업지원 프로그램 로드맵</h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>로드맵을 통해 학교 취업프로그램을 제공</h3>";
-        third.innerHTML = "<a class='list_content_link' href='https://job.dongguk.edu:450/program/event_program.asp?page=2&cate_code=08&' target='_blank'><h2>동국대학교 취업센터 -> 캠퍼스 리크루팅</h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>다양한 기업들의 온라인 채용 설명회를 신청하고 참여할 수 있는 취업센터 내 공간</h3>";
-        fourth.innerHTML = "<a class='list_content_link' href='https://www.dongguk.edu/mbs/kr/subview.jsp?id=kr_040000000000' target='_blank'><h2>동국대학교 메인홈페이지 </h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>취업/창업/역량/봉사 부분은 취업의 역량과 경험을 키울 수 있는 동아리, 봉사, IPP사업단 사이트 정보를 제공</h3>";
-        fifth.innerHTML = "<a class='list_content_link' href='https://ddp.dongguk.edu/site/main/index001?prevurl=https%3A%2F%2Fddp.dongguk.edu%2Flogin.jsp' target='_blank'><h2>동국대학교 드림패스</h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>학교 수업 외 다양한 동아리 워크숍 정보를 얻을 수 있으며, 나의 활동을 정리 기록할 수 있는 사이트(역량검사, 적성검사 등 다양한 검사 가능, 채용 정보 또한 얻을 수 있음)</h3>";
+        first.innerHTML = "<a class='list_content_link' href='https://ddp.dongguk.edu/site/main/index001' target='_blank'><h2>동국대학교 취업센터</h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>채용 정보, 교내 취업프로그램, 공모전 정보 등을 제공</h3>";
+        second.innerHTML = "<a class='list_content_link' href='https://ddp.dongguk.edu/site/homepage/menu/viewMenu?menuid=001005002003' target='_blank'><h2>동국대학교 취업지원 프로그램 로드맵</h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>단계별 취업프로그램을 한눈에 알아볼 수 있도록 제공</h3>";
+        third.innerHTML = "<a class='list_content_link' href='https://ddp.dongguk.edu/site/program/recruit/listCampusRecruit?menuid=001005002002&recruittype=CR' target='_blank'><h2>동국대학교 취업센터 -> 캠퍼스 리크루팅</h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>다양한 기업들의 온라인 채용 설명회를 신청하고 참여할 수 있는 취업센터 내 공간</h3>";
+        fourth.innerHTML = "<a class='list_content_link' href='https://www.dongguk.edu/mbs/kr/subview.jsp?id=kr_040000000000' target='_blank'><h2>동국대학교 메인홈페이지 </h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>취업/창업/역량/봉사/IPP 사업단 등 취업 관련 공지 사항 및 학사 정보를 제공</h3>";
+        fifth.innerHTML = "<a class='list_content_link' href='https://ddp.dongguk.edu/site/main/index001?prevurl=https%3A%2F%2Fddp.dongguk.edu%2Flogin.jsp' target='_blank'><h2>동국대학교 드림패스</h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>학교 수업 외 다양한 정보를 얻을 수 있으며, 나의 활동을 정리 및 기록 가능</h3>";
         sixth.innerHTML = "";
     }
     if(event === 2){
@@ -31,11 +31,11 @@ function paintList(event){
         category_second.style.color = "orange";
         category_third.style.color = "black";
         category_fourth.style.color = "black";
-        first.innerHTML = "<a class='list_content_link' href='https://www.inflearn.com/' target='_blank'><h2>인프런</h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>다양한 직무의 인강을 들을 수 있으며 저렴한 가격과 초보 입문자들을 위한 교육이 많음, 무료 강의도 있지만 한정적</h3>";
-        second.innerHTML = "<a class='list_content_link' href='https://fastcampus.co.kr/' target='_blank'><h2>패스트캠퍼스</h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>실무자들이 직접 제공하는 인강으로 현직자들의 노하우를 배울 수 있는 사이트 => but, 전문성이 다소 떨어지는 인강이 있으며 가격이 비싼 편</h3>";
-        third.innerHTML = "<a class='list_content_link' href='https://tacademy.skplanet.com/live/player/listCategory.action' target='_blank'><h2>T 아카데미</h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>skt에서 주관하는 ict 전문인재 양성을 위한 인강프로그램, 빅데이터부터 서비스기획, 디자인까지 대기업 주관으로 인강의 신뢰성이 높다</h3>";
-        fourth.innerHTML = "<a class='list_content_link' href='https://www.boostcourse.org/' target='_blank'><h2>부스트코스</h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>네이버 지원으로 이루어진 IT관련 인강 기초 코딩과 디지털마케팅 수업을 들을 수 있으며 많은 무료 인강과 직접 프로젝트를 해 볼수 있는 기회가 장점</h3>";
-        fifth.innerHTML = "<a class='list_content_link' href='http://www.itbtm.com/main2/intro.php' target='_blank'><h2>유동근의 IT버팀목</h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>컴퓨터 활용 능력 시험 인강 => 유동근 강사의 노하우와 커리큘럼으로 높은 합격률을 보인다.</h3>";
+        first.innerHTML = "<a class='list_content_link' href='https://www.inflearn.com/' target='_blank'><h2>인프런</h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>다양한 직무의 입문자들을 위한 강의가 많으며, 한정적인 무료 강의를 제공</h3>";
+        second.innerHTML = "<a class='list_content_link' href='https://fastcampus.co.kr/' target='_blank'><h2>패스트캠퍼스</h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>현직자들의 노하우를 배울 수 있는 강의가 많으며, 전문성이 떨어지거나 가격이 비싼 경우가 있음</h3>";
+        third.innerHTML = "<a class='list_content_link' href='https://tacademy.skplanet.com/live/player/listCategory.action' target='_blank'><h2>T 아카데미</h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>SK Planet에서 운영하는 ICT 전문 인재 양성을 위한 온라인 강의 플랫폼</h3>";
+        fourth.innerHTML = "<a class='list_content_link' href='https://www.boostcourse.org/' target='_blank'><h2>부스트코스</h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>Naver Connect가 운영하는 프로젝트 기반의 IT 관련 무료 온라인 강의 플랫폼</h3>";
+        fifth.innerHTML = "<a class='list_content_link' href='http://www.itbtm.com/main2/intro.php' target='_blank'><h2>유동근의 IT버팀목</h2></a><h3 style='opacity: 0.5; margin-left: 10px;'>컴퓨터 활용 능력 시험 온라인 강의 사이트</h3>";
         sixth.innerHTML = "";
     }
     if(event === 3){        
