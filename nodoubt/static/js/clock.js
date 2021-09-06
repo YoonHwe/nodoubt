b26a067a2a13e0a1c0a2b1bd245ff831053f57a8
YoonHwe <dbsghl676@gmail.com>
2021-09-07 00:01:35 +0900

    2주차 시작

--- nodoubt/static/js/clock.js
+++ nodoubt/static/js/clock.js
@@ -1,30 +1,27 @@
 const clockTitle = document.querySelector(".content_second_dtime_clock");
 function getClock() {
-    const contentPopular = document.querySelector(".content_popular_top");
-    const contentPopularLast = contentPopular.lastElementChild;
-    const targetYear = contentPopularLast.childNodes[5];
-    const date = new Date();
-    const targetDay = new Date(2021, 8, 9, 0, 0, 0);
-    const month = String(date.getMonth()).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-    const hour = String(date.getHours()).padStart(2, "0");
-    const minute = String(date.getMinutes()).padStart(2, "0");
-    const second = String(date.getSeconds()).padStart(2, "0");
-    date.setMonth(month);
-    date.setDate(day);
-    date.setHours(hour);
-    date.setMinutes(minute);
-    date.setSeconds(second);
-    const timeGap = targetDay - date;
+    const promiseDay = new Date();
+    const targetDay = new Date(2021, 8, 13, 0, 0, 0);
+    const month = String(promiseDay.getMonth()).padStart(2, "0");
+    const day = String(promiseDay.getDate()).padStart(2, "0");
+    const hour = String(promiseDay.getHours()).padStart(2, "0");
+    const minute = String(promiseDay.getMinutes()).padStart(2, "0");
+    const second = String(promiseDay.getSeconds()).padStart(2, "0");
+    promiseDay.setMonth(month);
+    promiseDay.setDate(day);
+    promiseDay.setHours(hour);
+    promiseDay.setMinutes(minute);
+    promiseDay.setSeconds(second);
+    const timeGap = targetDay - promiseDay;
     const dayGap = Math.floor(timeGap / (1000 * 60 * 60 * 24));
     const hourGap = Math.floor(timeGap / (1000 * 60 * 60)) % 24;
     const minuteGap = Math.floor(timeGap / (1000 * 60)) % 60;
     const secondGap = Math.floor(timeGap / 1000) % 60;
     if(timeGap <= 0){
-        targetDay.innerText = "D - Day"
+        clockTitle.innerText = "D - Day"
     }
     else{
-        targetYear.innerText = `D - ${dayGap}일 ${hourGap}시간 ${minuteGap}분 ${secondGap}초`;
+        clockTitle.innerText = `D - ${dayGap}일 ${hourGap}시간 ${minuteGap}분 ${secondGap}초`;
     }
 }
 
