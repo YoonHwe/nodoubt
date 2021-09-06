d344cd0e0ee37be4db7e87202773a69e96dd3ada
YoonHwe <dbsghl676@gmail.com>
2021-09-06 21:42:31 +0900

    db reset

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Data, Feedback, Request
+from .models import Data, Feedback, Request, Like
 # Register your models here.
 admin.site.register(Data)
 admin.site.register(Request)
 admin.site.register(Feedback)
+admin.site.register(Like)
