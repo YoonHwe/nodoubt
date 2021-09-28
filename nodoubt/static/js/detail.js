7a0c4d4c4634cf954417c0377c12db8f77a779db
YoonHwe <dbsghl676@gmail.com>
2021-09-28 19:26:51 +0900

    detail 로딩 효과 추가

--- nodoubt/static/js/detail.js
+++ nodoubt/static/js/detail.js
@@ -1,2 +1,12 @@
 const body = document.querySelector("#detail_body");
-body.innerHTML = body.innerText;
+const tail = document.querySelector("#detail_tail");
+const loader = document.querySelector(".loader");
+tail.style.height= "50vh";
+body.style.display = "none";
+tail.style.visibility = "hidden";
+setTimeout( () => {
+    body.innerHTML = body.innerText;
+    body.style.display = "block";
+    tail.style.visibility = "visible";
+    loader.style.display = "none";
+}, 1000);
