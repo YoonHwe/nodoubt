d104e4c040555154dbfb1562266396dcd6d11085
YoonHwe <dbsghl676@gmail.com>
2021-10-12 19:01:18 +0900

    [NODOUBT] 10월 2주차 자료 전달 완료 & [특별 공개 자료] 익스플즌 공개

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Data, Feedback, Request, Like
 # Register your models here.
-admin.site.register(Data)
+class dataAdmin(admin.ModelAdmin):
+    search_fields = ['company']
+
+admin.site.register(Data, dataAdmin)
 admin.site.register(Request)
 admin.site.register(Feedback)
 admin.site.register(Like)
+
