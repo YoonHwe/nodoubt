a8e29e54a0bee4fbd729fa29c44902935c59a4f0
YoonHwe <dbsghl676@gmail.com>
2022-02-23 20:39:36 +0900

    Update urls.py

--- nodoubt/urls.py
+++ nodoubt/urls.py
@@ -20,8 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
     #allauth의 url 연결
     path('accounts/', include('allauth.urls')),
+    path('', include('main.urls')),
     path('users/', include('users.urls')),
 ]  + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
